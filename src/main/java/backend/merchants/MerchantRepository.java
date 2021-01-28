0b78a9f580cf874fcf7f6fdb2c84f574b737ee44
AnthAlum <anthony.quispdecr@alum.uca.es>
2021-01-28 14:20:43 +0100

    Cambio de tipo Integer a Long en las interfaces CRUD de Merchant y Client

--- src/main/java/backend/merchants/MerchantRepository.java
+++ src/main/java/backend/merchants/MerchantRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MerchantRepository extends CrudRepository<Merchant, Integer> {
+public interface MerchantRepository extends CrudRepository<Merchant, Long> {
 }
